7e3f60de1a3008564b0fc4bdc0b22b387569b264
Евгений <gekachan2000@gmail.com>
2021-05-21 19:49:19 +0500

    Заготовка для демона расчета маршрутов

---
+++ RoutePlannerDaemon/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using GeneticAlgorithm.Contracts;
+using GeneticAlgorithm.Domain;
+using SimpleInjector;
+
+namespace RoutePlannerDaemon
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var container = new Container();
+            
+            container.RegisterSingleton<IPopulationGenerator, PopulationGenerator>();
+            // реализовать routeStepCalculator 
+            container.RegisterSingleton<IRouteStepCalculator, FakeRouteStepCalculator>();
+            container.RegisterSingleton<IRouteParametersCalculator, RouteParametersCalculator>();
+            
+            container.RegisterSingleton<IFitnessCalculator, FitnessCalculator>();
+            container.RegisterSingleton<IGenerationRanker, GenerationRanker>();
+            
+            container.RegisterSingleton<IGenerationSelector, GenerationSelector>();
+            container.RegisterSingleton<IGenerationBreeder, GenerationBreeder>();
+            container.RegisterSingleton<IGenerationMutator, GenerationMutator>();
+            container.RegisterSingleton<IGenerationCreator, GenerationCreator>();
+            
+            container.RegisterSingleton<IRoutePlanner, RoutePlanner>();
+            
+            container.Register<RoutesUpdater>();
+
+            container.Verify();
+
+            var routesUpdater = container.GetInstance<RoutesUpdater>();
+            await routesUpdater.RunAsync();
+        }
+    }
+}
---
+++ RoutePlannerDaemon/RoutesUpdater.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+
+namespace RoutePlannerDaemon
+{
+    public class RoutesUpdater
+    {
+        public async Task RunAsync()
+        {
+            
+        }
+    }
+}
