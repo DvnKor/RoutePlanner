c6e45867979cf675e448e327e3b1a2448e565e6c
Евгений <gekachan2000@gmail.com>
2021-05-23 20:33:42 +0500

    Обернул итерацию демона в try catch

--- RoutePlannerDaemon/RoutesUpdater.cs
+++ RoutePlannerDaemon/RoutesUpdater.cs
@@ -35,52 +35,64 @@ namespace RoutePlannerDaemon
         {
             while (true)
             {
-                var now = DateTime.UtcNow.AddHours(5);
-                Console.WriteLine($"Началось обновление маршрутов {now:g}");
-
-                var algorithmStartTime = now + _reserveMeetingTime;
-                var meetings = await _meetingStorage.GetMeetings(algorithmStartTime);
-                
-                var managerSchedules = await _managerScheduleStorage.GetManagerSchedules(now);
-                var managerSchedulesIds = managerSchedules
-                    .Select(managerSchedule => managerSchedule.Id)
-                    .ToArray();
-                var currentRoutes = await _routeStorage.GetRoutes(managerSchedulesIds);
-
-                foreach (var managerSchedule in managerSchedules)
+                try
                 {
-                    var routeExist = currentRoutes.TryGetValue(managerSchedule.Id, out var route);
-                    if (routeExist)
-                    {
-                        var currentMeeting = route.SuitableMeetings
-                            .Where(meeting => meeting.StartTime <= algorithmStartTime)
-                            .OrderByDescending(meeting => meeting.StartTime)
-                            .FirstOrDefault();
-                        if (currentMeeting != null)
-                        {
-                            managerSchedule.StartTime = currentMeeting.EndTime;
-                            managerSchedule.StartCoordinate = currentMeeting.Coordinate;
-                        }
-                    } 
+                    await UpdateRoutes();
                 }
-            
-                var bestRoutes = _routePlanner.GetBestRoutes(
-                    managerSchedules,
-                    meetings,
-                    GenerationCount,
-                    PopulationSize,
-                    EliteSize,
-                    MutationRate);
-
-                Console.WriteLine(bestRoutes.PrintRoutesWithParameters());
-                
-                foreach (var route in bestRoutes.Routes)
+                catch (Exception ex)
                 {
-                    await _routeStorage.AddOrUpdateRoute(route);
+                    Console.WriteLine(ex);
                 }
-                
-                //toDo удалять старые маршруты
             }
+        }
+
+        private async Task UpdateRoutes()
+        {
+            var now = DateTime.UtcNow.AddHours(5);
+            Console.WriteLine($"Началось обновление маршрутов {now:g}");
+
+            var algorithmStartTime = now + _reserveMeetingTime;
+            var meetings = await _meetingStorage.GetMeetings(algorithmStartTime);
+
+            var managerSchedules = await _managerScheduleStorage.GetManagerSchedules(now);
+            var managerSchedulesIds = managerSchedules
+                .Select(managerSchedule => managerSchedule.Id)
+                .ToArray();
+            var currentRoutes = await _routeStorage.GetRoutes(managerSchedulesIds);
+
+            foreach (var managerSchedule in managerSchedules)
+            {
+                var routeExist = currentRoutes.TryGetValue(managerSchedule.Id, out var route);
+                if (routeExist)
+                {
+                    var currentMeeting = route.SuitableMeetings
+                        .Where(meeting => meeting.StartTime <= algorithmStartTime)
+                        .OrderByDescending(meeting => meeting.StartTime)
+                        .FirstOrDefault();
+                    if (currentMeeting != null)
+                    {
+                        managerSchedule.StartTime = currentMeeting.EndTime;
+                        managerSchedule.StartCoordinate = currentMeeting.Coordinate;
+                    }
+                }
+            }
+
+            var bestRoutes = _routePlanner.GetBestRoutes(
+                managerSchedules,
+                meetings,
+                GenerationCount,
+                PopulationSize,
+                EliteSize,
+                MutationRate);
+
+            Console.WriteLine(bestRoutes.PrintRoutesWithParameters());
+
+            foreach (var route in bestRoutes.Routes)
+            {
+                await _routeStorage.AddOrUpdateRoute(route);
+            }
+
+            //toDo удалять старые маршруты 
         }
     }
 }
