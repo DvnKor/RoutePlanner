b53b997f055fa1c3b09d622d2a632bc82455413b
Евгений <gekachan2000@gmail.com>
2021-05-26 08:22:09 +0500

    Удалил неиспользуемый класс

--- Infrastructure.Cache/CacheFactory.cs
+++ Infrastructure.Cache/CacheFactory.cs
@@ -4,12 +4,6 @@ namespace Infrastructure.Cache
 {
     public static class CacheFactory
     {
-        public static InstanceLifetimeCache<TKey, TValue> CreateInstanceLifetimeCache<TKey, TValue>(
-            Func<TKey, TValue> valueProvider)
-        {
-            return new InstanceLifetimeCache<TKey, TValue>(valueProvider);
-        }
-        
         public static ExpiringCache<TKey, TValue> CreateExpiringCache<TKey, TValue>(
             Func<TKey, TValue> valueProvider, int cacheExpirationMinutes)
         {
