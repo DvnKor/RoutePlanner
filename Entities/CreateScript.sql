db60065a60ed41989f7ed37efd3e291614a1a504
Евгений <gekachan2000@gmail.com>
2021-05-29 16:18:26 +0500

    Update CreateScript.sql

--- Entities/CreateScript.sql
+++ Entities/CreateScript.sql
@@ -33,6 +33,7 @@ CREATE TABLE "Meeting" (
                            "ClientId" integer NOT NULL,
                            "AvailableTimeStart" timestamp without time zone NOT NULL,
                            "AvailableTimeEnd" timestamp without time zone NOT NULL,
+						   "Duration" double precision NOT NULL,
                            "StartTime" timestamp without time zone NOT NULL,
                            "EndTime" timestamp without time zone NOT NULL,
                            "Coordinate" text NULL,
